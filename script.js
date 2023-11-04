61e36e6107dd54f3760b47ceb3bbc657b35f271a
lalitha-sahitya <lalithasahitya.palacharla@gmail.com>
2023-11-04 22:17:35 +0530

    score

--- script.js
+++ script.js
@@ -4,12 +4,15 @@ const lost=document.getElementById('loose');
 const resetBtn=document.getElementById('reset');
 const winMsg=document.getElementById('win');
 const resetBtn2=document.getElementById('re-set');
+const scoreDisplay=document.getElementById('score');
+const leastMoves=document.getElementById('least-moves');
 let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
 let moves=0;
 let score=0;
-
+let lessMoves=20;
+alert("You have 20 moves only!!");
 function flipCard() {
     if (lockBoard || this === firstCard) return;
     
@@ -21,6 +24,10 @@ function flipCard() {
     } else {
         moves++;
         noOfMoves.textContent = `Moves : ${moves}`;
+        if(moves<lessMoves||lessMoves===0){
+          lessMoves=moves;
+          leastMoves.textContent=`Least Moves: ${lessMoves}`;
+        }
       secondCard = this;
       checkForMatch();
       if(moves>20){
@@ -37,6 +44,7 @@ function checkForMatch() {
 
   if (firstCardImage === secondCardImage) {
     score++;
+    scoreDisplay.textContent=`Score : ${score}/8`;
     disableCards();
     if(score==8){
         winMsg.style.display="block";
@@ -58,7 +66,6 @@ function unflipCards() {
   setTimeout(() => {
     firstCard.classList.remove('flip');
     secondCard.classList.remove('flip');
-
     resetBoard();
   }, 1000);
 }
@@ -87,6 +94,7 @@ function resetGame() {
   moves = 0;
   score = 0;
   noOfMoves.textContent = `Moves: ${moves}`;
+  scoreDisplay.textContent=`Score : ${score}/8`;
   lost.style.display = "none";
   winMsg.style.display = "none";
   resetBoard();
